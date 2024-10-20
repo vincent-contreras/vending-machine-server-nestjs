3877596e35b1415f956468aace47eb5fe074be9f
Paul Vincent Contreras <vincent.contreras@outlook.com>
2024-10-21 08:33:09 +0900

    + Use environment variables for DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { Module } from '@nestjs/common';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { ButtonsModule } from './buttons/buttons.module';
-import { ButtonSchema } from './buttons/entities/button.entity';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017',{dbName: 'test'}),
+    MongooseModule.forRoot('mongodb://localhost:27017',{dbName: process.env.DB_NAME }),
     ButtonsModule
   ]
 })

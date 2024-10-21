7cbd7e629f42bcd64e5646be9db153f7948d7bde
Paul Vincent Contreras <vincent.contreras@outlook.com>
2024-10-21 10:41:14 +0900

    + Add admin login and remove unwanted code

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '300s'}
+    }),
+  ],
+  providers: [
+    AuthService
+  ],
+  controllers: [AuthController],
+  exports: [AuthService]
+})
+export class AuthModule {}

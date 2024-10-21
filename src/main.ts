434c17fce4b17e775a61e7d699dd063863e5f021
Paul Vincent Contreras <vincent.contreras@outlook.com>
2024-10-21 10:11:06 +0900

    + Fix DTO validation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerConfig } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 const { BASE_URL, APP_PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(BASE_URL);
+  
+  app.useGlobalPipes(new ValidationPipe());
 
   app.enableVersioning();
 
